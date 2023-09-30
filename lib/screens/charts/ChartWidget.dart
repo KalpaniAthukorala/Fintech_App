0c3a83183f5a377b42a505ea50158fd5198a85b5
Kalpani Athukorala <kalpaniathukorala1997@gmail.com>
2023-09-30 15:21:49 +0530

    Added-details-page

---
+++ lib/screens/charts/ChartWidget.dart
@@ -0,0 +1,29 @@
+// import 'package:flutter/material.dart';
+// import 'package:fl_chart/fl_chart.dart';
+
+// class ChartWidget extends StatelessWidget {
+//   final double incomeAmount;
+//   final double expensesAmount;
+
+//   ChartWidget({required this.incomeAmount, required this.expensesAmount});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return PieChart(
+//       PieChartData(
+//         sections: [
+//           PieChartSectionData(
+//             title: 'Income',
+//             color: Colors.green,
+//             value: incomeAmount,
+//           ),
+//           PieChartSectionData(
+//             title: 'Expenses',
+//             color: Colors.red,
+//             value: expensesAmount,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
